6de05978bff63107d58b294448e8a277b0091869
Daniel Gleason <danielg@surveymonkey.com>
2018-11-30 18:11:39 -0800

    fixed formatting pep8 issues

--- functions.py
+++ functions.py
@@ -1,18 +1,18 @@
 import numpy
-import binascii
 from PIL import Image, ImageOps
 
-#chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz'
+# chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz'
 startAsciiRange = 32
 endAsciiRange = 122
-chars = "".join([chr(x) for x in range(startAsciiRange, endAsciiRange+1)])
+chars = "".join([chr(x) for x in range(startAsciiRange, endAsciiRange + 1)])
 defaultBase = 5
 defaultResizeFactor = 4
+
 
 def char_to_num(c, charMap=chars):
     if charMap:
         alpha = dict()
-        for i,v in enumerate(charMap):
+        for i, v in enumerate(charMap):
             alpha[v] = i
         try:
             return alpha[c]
@@ -21,24 +21,29 @@ def char_to_num(c, charMap=chars):
     else:
         return ord(c)
 
+
 def num_to_bits(x, base=defaultBase, numBits=None):
-    if numBits and x>=(base**numBits):
-        raise RuntimeError("Insufficient bits to hold %i with base %i and %i bits. Raise base or numBits." % (x, base, numBits))
+    if numBits and x >= (base**numBits):
+        raise RuntimeError(
+            "Insufficient bits to hold %i with base %i and %i bits. Raise base or numBits." % (x, base, numBits))
     digits = []
     while x:
-        digits.append(x%base)
+        digits.append(x % base)
         x /= base
     if numBits:
-        digits += [0]*(numBits-len(digits))
+        digits += [0] * (numBits - len(digits))
     digits.reverse()
     return digits
+
 
 def char_to_bits(c, base=defaultBase, numBits=None, charMap=chars):
     try:
         return num_to_bits(char_to_num(c, charMap), base, numBits)
     except RuntimeError, e:
         print e
-        raise RuntimeError("Unable to hold %c's position (%i) in charmap [%s] with base %i and %i bits. Raise base, numBits, or change charMap." % (c, charMap.find(c), charMap, base, numBits))
+        raise RuntimeError("Unable to hold %c's position (%i) in charmap [%s] with base %i and %i bits. Raise base, numBits, or change charMap." % (
+            c, charMap.find(c), charMap, base, numBits))
+
 
 def line_to_bit_array(l, base=defaultBase, numBits=None, charMap=chars):
     # We need to figure out the numbits for the maximum char
@@ -46,6 +51,7 @@ def line_to_bit_array(l, base=defaultBase, numBits=None, charMap=chars):
         maxChar = max([chars.find(c) for c in s])
         numBits = len(num_to_bits(maxChar, base))
     return [char_to_bits(c, base, numBits, charMap) for c in l]
+
 
 def string_to_array(s, base=defaultBase, numBits=None, charMap=chars, isHorizontal=False, addSpaces=False):
 
@@ -85,35 +91,41 @@ def string_to_array(s, base=defaultBase, numBits=None, charMap=chars, isHorizont
         if isHorizontal:
             returnMe = numpy.vstack([row.T for row in reshaped])
             if addSpaces:
-                splitArray = numpy.vsplit(returnMe, returnMe.shape[0]/numBits)
+                splitArray = numpy.vsplit(returnMe, returnMe.shape[0] / numBits)
                 # Add spaces between the split sub-arrays
-                returnMe = numpy.vstack([numpy.vstack([section, [0]*numChars]) for section in splitArray])[:-1]
+                returnMe = numpy.vstack([numpy.vstack([section, [0] * numChars]) for section in splitArray])[:-1]
         else:
             returnMe = numpy.hstack(reshaped)
             if addSpaces:
-                splitArray = numpy.hsplit(returnMe, returnMe.shape[1]/numBits)
+                splitArray = numpy.hsplit(returnMe, returnMe.shape[1] / numBits)
                 # Add spaces between the split sub-arrays
-                returnMe = numpy.hstack([numpy.vstack([numpy.hstack([item,[0]]) for item in section]) for section in splitArray])[:,:-1]
+                returnMe = numpy.hstack([numpy.vstack([numpy.hstack([item, [0]]) for item in section])
+                                         for section in splitArray])[:, :-1]
 
         # Stack the array and return
         return returnMe
+
 
 def string_to_image(s, base=defaultBase, numBits=None, charMap=chars, isHorizontal=False):
     arr = string_to_array(s, base, numBits, charMap, isHorizontal, True)
     scaled = scale_array(arr, base=base)
     return Image.fromarray(scaled, "L")
 
+
 def scale_array(array, scale=255, base=defaultBase):
-    return (array/float(base-1)*float(scale)).astype(numpy.uint8)
+    return (array / float(base - 1) * float(scale)).astype(numpy.uint8)
+
 
 def resize(image, resizeFactor=defaultResizeFactor):
-    newWidth = image.size[1]*resizeFactor
-    newHeight = image.size[0]*resizeFactor
+    newWidth = image.size[1] * resizeFactor
+    newHeight = image.size[0] * resizeFactor
     resized = image.resize((newHeight, newWidth))
     return resized
 
+
 def border(image, color="black", width=1):
-    return ImageOps.expand(image.convert("RGB"),border=width, fill=color)
+    return ImageOps.expand(image.convert("RGB"), border=width, fill=color)
+
 
 def speech_bubble(s, base=defaultBase, numBits=None, charMap=chars, isHorizontal=False, fgColor="white", bgColor="black", resizeFactor=defaultResizeFactor):
     img = string_to_image(s, base, numBits, charMap, isHorizontal)
@@ -122,7 +134,10 @@ def speech_bubble(s, base=defaultBase, numBits=None, charMap=chars, isHorizontal
     img = resize(img, resizeFactor)
     return img
 
-s = r"""In a field one summer's day a Grasshopper was
+
+if __name__ == '__main__':
+
+    s = r"""In a field one summer's day a Grasshopper was
 hopping about, chirping and singing to its heart's
 content. An Ant passed by, bearing along with great
 toil an ear of corn he was taking to the nest.
@@ -143,19 +158,19 @@ collected in the summer.
 
 Then the Grasshopper knew..."""
 
-#s = "".join([c for c in chars])
+# s = "".join([c for c in chars])
 
-alienGreen = (150,200,100)
+    alienGreen = (150, 200, 100)
 
-img1 = speech_bubble(s, fgColor=alienGreen)
-img2 = speech_bubble(s, fgColor="red", isHorizontal=True)
-img3 = speech_bubble(s, base=3, fgColor=(0,128,255))
+    img1 = speech_bubble(s, fgColor=alienGreen)
+    img2 = speech_bubble(s, fgColor="red", isHorizontal=True)
+    img3 = speech_bubble(s, base=3, fgColor=(0, 128, 255))
 
-img1.save("img1.png")
-img2.save("img2.png")
-img3.save("img3.png")
+    img1.save("img1.png")
+    img2.save("img2.png")
+    img3.save("img3.png")
 
-r = """Kraken
-Keyboards"""
+    r = """Kraken
+    Keyboards"""
 
-speech_bubble(r, isHorizontal=True, fgColor=alienGreen).save('kraken_keyboards.png')
+    speech_bubble(r, isHorizontal=True, fgColor=alienGreen).save('kraken_keyboards.png')
